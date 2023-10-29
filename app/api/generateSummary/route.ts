0fd89b1e64d0ee35ff4b12bf37f43edb0eee65d9
unknown <deysarkarrohan@gmail.com>
2023-10-29 17:54:34 +0530

    updating cb

---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,32 @@
+import openai from "@/openai";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const { todos } = await request.json();
+    console.log(todos);
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        temperature: 0.8,
+        n: 1,
+        stream: false,
+        messages: [
+            {
+                role: "system",
+                content: "when responding, welcome the user always"
+            },
+            {
+                role: "user",
+                content: `Hi there, provide summary of the following todos. Count
+                how many todos are there in each category such as To do, in progress and done
+                then tell user to have a productive day! Here's the data ${JSON.stringify(todos)}`
+            },
+        ]
+    });
+
+    const data = response.choices[0].message;
+
+    console.log(data);
+
+    return NextResponse.json(data.choices[0].message)
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,35 @@
 import Image from "next/image"
 import {MagnifyingGlassIcon, UserCircleIcon} from "@heroicons/react/24/solid"
 import Avatar from "react-avatar"
-
+import { useBoardStore } from "@/store/BoardStore";
+import {useEffect, useState} from "react";
+import fetchSuggestion from "@/lib/fetchSuggestion";
 
 function Header() {
-  return (
+    const [board, searchString, setSearchString] = useBoardStore((state) => [
+        state.board,
+        state.searchString,
+        state.setSearchString
+    ]);
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [suggestion, setSuggestion] = useState<string>("");
+
+    useEffect(() => {
+        if (board.columns.size === 0) return ;
+        setLoading(true)
+
+        const fetchSuggestionFunc = async() => {
+            const suggestion = await fetchSuggestion(board);
+            setSuggestion(suggestion)
+            setLoading(false)
+        }
+
+        fetchSuggestionFunc();
+    }, [board])
+    
+
+    return (
     <header>
         <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
             
@@ -18,8 +43,8 @@ function Header() {
                     w-full 
                     h-96 
                     bg-gradient-to-br 
-                    from-pink-400 
-                    to-[#0055D1] 
+                    from-[#3492ff]
+                    to-[#f155ff] 
                     rounded-md filter 
                     blur-3xl o
                     pacity-50 
@@ -42,7 +67,13 @@ function Header() {
                     <MagnifyingGlassIcon 
                         className="h-6 w-6 text-gray-400"
                     />
-                    <input type="text" placeholder="Search" className="flex-1 outline-none p-2"/>
+                    <input 
+                        type="text" 
+                        placeholder="Search" 
+                        className="flex-1 outline-none p-2"
+                        value={searchString}
+                        onChange={e => setSearchString(e.target.value)}
+                    />
                     <button type="submit" hidden>Search</button>
                 </form>
 
@@ -57,14 +88,13 @@ function Header() {
         <div className="flex items-center justify-center px-5 py-2 md:py-5">
             <p className="flex items-center text-sm font-light pr-5 p-5 shadow-xl rounded-xl w-fit  bg-white italic max-w-3xl text-[#0055D1]">
                 <UserCircleIcon
-                    className="inline-block h-10 w-10 text-[#0055D1] mr-1"
+                    className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"}`}
                 />
-
-                GPT is blah blah...
+                {suggestion && !loading ? suggestion : "Summarising your tasks..."}
             </p>
         </div>
     </header>
-  )
+    )
 }
 
 export default Header
